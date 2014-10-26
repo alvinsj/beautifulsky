43ef252be1ff7e5108b8e888309afe6de8118db8
alvinsj <email.to.alvin@gmail.com>
2014-10-26 15:37:07 +0700

    minor refactoring, and store tweets info in redis

--- beautifulsky.go
+++ beautifulsky.go
@@ -1,172 +1,87 @@
 package main
 
 import (
-	"os"
-	"github.com/gin-gonic/gin"
- 	"github.com/kurrik/oauth1a"
-	"github.com/kurrik/twittergo"
-	"net/http"
-	"net/url"
-	"time"
-	"fmt"
 	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+	"github.com/kurrik/twittergo"
+	"os"
+    "beautifulsky/twitter"
 )
 
-
-
-func LoadCredentials() (client *twittergo.Client, err error) {
-	config := &oauth1a.ClientConfig{
-		ConsumerKey:    os.Getenv("TWITTER_CONSUMER_KEY"),
-		ConsumerSecret: os.Getenv("TWITTER_CONSUMER_SECRET"),
-	}
-	user := oauth1a.NewAuthorizedConfig(os.Getenv("TWITTER_ACCESS_TOKEN"), os.Getenv("TWITTER_ACCESS_KEY"))
-	client = twittergo.NewClient(config, user)
-	return
-}
-
-
-
-func PrintTweets(c *gin.Context, k chan *twittergo.SearchResults, t chan map[string]string, done chan bool){
-
-	fmt.Printf("start PrintTweets \n")
-	results := <- k
-	for _, tweet := range results.Statuses() {
-		user := tweet.User()
-		entities := tweet["entities"].(map[string]interface{})
-		urls := entities["urls"].([]interface{})
-
-		if(len(urls) > 0){
-			url := urls[0].(map[string]interface {})
-
-			resp := make(map[string]string)
-			resp["tweet"] = fmt.Sprintf("%v", tweet.Text())
-			resp["image"] = fmt.Sprintf(" %v", url["expanded_url"])
-			resp["user"] = fmt.Sprintf("%v (@%v) ", user.Name(), user.ScreenName())
-			resp["created"] = fmt.Sprintf("%v", tweet.CreatedAt().Format(time.RFC1123))
-			t <- resp
-		}
-	}
-
-	fmt.Printf("end PrintTweets \n")
-	close(t)
-}
-
-
-
-func GetTweets(client *twittergo.Client, k chan *twittergo.SearchResults, r chan *twittergo.APIResponse){
-	var (
-		err error
-		results *twittergo.SearchResults
-		req     *http.Request
-		resp    *twittergo.APIResponse
-	)
-
-	query := url.Values{}
-	query.Set("q", "#beautifulsky")
-	query.Set("result_type", "recent")
-	query.Set("count","100")
-	url := fmt.Sprintf("/1.1/search/tweets.json?%v", query.Encode())
-	req, err = http.NewRequest("GET", url, nil)
-	if err != nil {
-		fmt.Printf("Could not parse request: %v\n", err)
-		os.Exit(1)
-	}
-
-	resp, err = client.SendRequest(req)
-	
-	if err != nil {
-		fmt.Printf("Could not send request: %v\n", err)
-		os.Exit(1)
-	}
-
-	results = &twittergo.SearchResults{}
-	
-	err = resp.Parse(results)
-	if err != nil {
-		fmt.Printf("Problem parsing response: %v\n", err)
-		os.Exit(1)
-	}
-	k <- results
-	r <- resp
-}
-
-
-
-func PrintRateLimit(r chan *twittergo.APIResponse){
-	fmt.Printf("start PrintRateLimit \n")
-	resp := <- r
-	if resp.HasRateLimit() {
-		fmt.Printf("Rate limit:           %v\n", resp.RateLimit())
-		fmt.Printf("Rate limit remaining: %v\n", resp.RateLimitRemaining())
-		fmt.Printf("Rate limit reset:     %v\n", resp.RateLimitReset())
-	} else {
-		fmt.Printf("Could not parse rate limit from response.\n")
-	}
-	fmt.Printf("end PrintRateLimit \n")
-}
-
-
-
 func main() {
-    r := gin.Default()
+	r := gin.Default()
 
-    pwd, err := os.Getwd()
+	pwd, err := os.Getwd()
 	if err != nil {
-	    panic(err)
+		panic(err)
 	}
 
-    r.Use(static.Serve(pwd+"/frontend/public")) 
-	r.NoRoute(static.Serve(pwd+"/frontend/public")) 
+	r.Use(static.Serve(pwd + "/frontend/public"))
+	r.NoRoute(static.Serve(pwd + "/frontend/public"))
 
-    r.GET("/ping", func(c *gin.Context) {
-        c.String(200, "pong")
-    })
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
 
-    r.GET("/images", func(c *gin.Context) {
-    	var (
-			client  *twittergo.Client			
-		)
-		
+	r.GET("/images", func(c *gin.Context) {
+
 		k := make(chan *twittergo.SearchResults)
 		r := make(chan *twittergo.APIResponse)
 		t := make(chan map[string]string)
 		done := make(chan bool)
-
-		client, _ = LoadCredentials()
-		go GetTweets(client, k, r)
-		go PrintTweets(c, k, t, done)
-		go PrintRateLimit(r)
+		tweetsFromCacheDone := make(chan bool)
+        tw := twitter.Twitter{}
 		
-		c.Data(200, "application/json", []byte("["))
+		go tw.SearchTweets(k, r)
+		go tw.TweetsFromCache(t, tweetsFromCacheDone)
 
-		i := 0
+        // parse tweets from twitter 
 		go func() {
-	        for {
-	            resp, more := <- t
-	            if more {
-	                fmt.Println("received response")
-	                if i != 0 {
-	                	c.Data(200, "application/json", []byte(","))
-	                }
+            <- tweetsFromCacheDone
+			for {
+                results, more := <-k
 
-	               	jsonString, _ := json.Marshal(resp)
-	                c.Data(200, "application/json", []byte(jsonString))
-	                i++
+				if more {
+					tw.TweetsFromResults(c, results, t, done)
+				} else {
+					close(t)
+					return
+				}
+			}
 
-	            } else {
-	                fmt.Println("received all jobs")
-	                c.Data(200, "application/json", []byte("]"))
-	                done <- true
-	                return
-	            }
-	        }
-	    }()
+		}()
+		
+        // pipe results to response
+		go func() {
+            i := 0
+            c.Data(200, "application/json", []byte("["))
+			
+            for {
+				resp, more := <-t
+                if more {
+					fmt.Println("received response")
+					if i != 0 {
+						c.Data(200, "application/json", []byte(","))
+					}
+
+					jsonString, _ := json.Marshal(resp)
+					c.Data(200, "application/json", []byte(jsonString))
+					i++
+
+				} else {
+					fmt.Println("received all jobs")
+					c.Data(200, "application/json", []byte("]"))
+					done <- true
+					return
+				}
+			}
+		}()
 		<-done
-    })
+        tw.PrintRateLimit(r)
+	})
 
-    // Listen and server on 0.0.0.0:8080
-    r.Run(":"+os.Getenv("PORT"))
+	r.Run(":" + os.Getenv("PORT"))
 
 }
-
---
+++ twitter/twitter.go
@@ -0,0 +1,169 @@
+package twitter
+
+import (
+	"fmt"
+	"github.com/garyburd/redigo/redis"
+	"github.com/gin-gonic/gin"
+	"github.com/kurrik/oauth1a"
+	"github.com/kurrik/twittergo"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+)
+
+type Twitter struct{}
+
+var (
+	TWITTER_COSUMER_KEY     = os.Getenv("TWITTER_CONSUMER_KEY")
+	TWITTER_CONSUMER_SECRET = os.Getenv("TWITTER_CONSUMER_SECRET")
+	conn, redisErr          = redis.Dial("tcp", os.Getenv("REDISTOGO"))
+)
+
+
+func (tw Twitter) LoadCredentials() (client *twittergo.Client, err error) {
+	config := &oauth1a.ClientConfig{
+		ConsumerKey:    TWITTER_COSUMER_KEY,
+		ConsumerSecret: TWITTER_CONSUMER_SECRET,
+	}
+	user := oauth1a.NewAuthorizedConfig("", "")
+	client = twittergo.NewClient(config, user)
+	return
+}
+
+
+func (tw Twitter) Memoize(resp map[string]string, tweetId uint64, key string, value string) map[string]string {
+	cache, _ := redis.String(conn.Do("HGET", fmt.Sprintf("tweet:%v", tweetId), key))
+    if cache == "" {
+		conn.Do("HSET", fmt.Sprintf("tweet:%v", tweetId), key, value)
+		cache = value
+	}
+	resp[key] = cache
+	return resp
+}
+
+
+func (tw Twitter) TweetsFromResults(c *gin.Context, results *twittergo.SearchResults, t chan map[string]string, done chan bool) {
+
+	fmt.Printf("start TweetsFromResults \n")
+
+	for _, tweet := range results.Statuses() {
+
+		user := tweet.User()
+		entities := tweet["entities"].(map[string]interface{})
+		urls := entities["urls"].([]interface{})
+
+		if len(urls) > 0 {
+			url := urls[0].(map[string]interface{})
+
+			if redisErr != nil {
+				fmt.Printf("Redis error: %v\n", redisErr)
+			}
+
+			resp := make(map[string]string)
+
+			reply, _ := redis.Values(conn.Do("KEYS", fmt.Sprintf("tweet:%v", tweet.Id())))
+			fmt.Printf("values: %v", reply)
+			if len(reply) == 0 {
+				conn.Do("RPUSH", "tweets", tweet.Id())
+			}
+
+			resp = tw.Memoize(resp, tweet.Id(), "tweet", fmt.Sprintf("%v", tweet.Text()))
+			resp = tw.Memoize(resp, tweet.Id(), "image", fmt.Sprintf("%v", url["expanded_url"]))
+			resp = tw.Memoize(resp, tweet.Id(), "user", fmt.Sprintf("%v (@%v) ", user.Name(), user.ScreenName()))
+			resp = tw.Memoize(resp, tweet.Id(), "created", fmt.Sprintf("%v", tweet.CreatedAt().Format(time.RFC1123)))
+			t <- resp
+		}
+	}
+	fmt.Printf("end TweetsFromResults \n")
+}
+
+
+func (tw Twitter) RetrieveMaxId() (string, bool){
+	l, _ := redis.Int(conn.Do("LLEN", "tweets"))
+	reply, _ := redis.String( conn.Do("LINDEX", "tweets", l-1) )
+	if reply == "" {
+		return reply, false
+	} else {
+		return reply, true
+	}
+}
+
+
+func (tw Twitter) ConstructParams() url.Values {
+	query := url.Values{}
+	query.Set("q", "#beautifulsky")
+	query.Set("result_type", "mixed")
+	query.Set("count", "100")
+	
+	if maxId, present := tw.RetrieveMaxId(); present {
+		fmt.Printf("maxId: %v", maxId)
+		query.Set("max_id", maxId)
+	}
+	return query
+}
+
+
+func (tw Twitter) SearchTweets( k chan *twittergo.SearchResults, r chan *twittergo.APIResponse) {
+	var (
+		err     error
+		results *twittergo.SearchResults
+		req     *http.Request
+		resp    *twittergo.APIResponse
+		client *twittergo.Client
+	)
+	client, _ = tw.LoadCredentials()
+
+	query := tw.ConstructParams()
+
+	url := fmt.Sprintf("/1.1/search/tweets.json?%v", query.Encode())
+	req, err = http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Printf("Could not parse request: %v\n", err)
+	}
+
+	resp, err = client.SendRequest(req)
+	if err != nil {
+		fmt.Printf("Could not send request: %v\n", err)
+	}
+
+	results = &twittergo.SearchResults{}
+	err = resp.Parse(results)
+	if err != nil {
+		fmt.Printf("Problem parsing response: %v\n", err)
+	}
+
+	k <- results
+	close(k)
+	r <- resp
+}
+
+
+func (tw Twitter) TweetsFromCache(t chan map[string]string, cacheDone chan bool) {
+    conn, redisErr = redis.Dial("tcp", os.Getenv("REDISTOGO"))
+	l, _ := redis.Int(conn.Do("LLEN", "tweets"))
+	for i:=0; i< l; i++ {
+		tweetId, _ := redis.String(conn.Do("LINDEX", "tweets", i))
+		var resp map[string]string = make(map[string]string)
+		for _, key := range []string{"tweet","image","user","created"} {
+			reply,_ := redis.String( conn.Do("HGET", "tweet:"+tweetId, key) )
+			resp[key] = reply
+		}
+		t <- resp
+	}
+	cacheDone <- true
+}
+
+
+func (tw Twitter) PrintRateLimit(r chan *twittergo.APIResponse) {
+	fmt.Printf("start PrintRateLimit \n")
+	resp := <-r
+	if resp.HasRateLimit() {
+		fmt.Printf("Rate limit:           %v\n", resp.RateLimit())
+		fmt.Printf("Rate limit remaining: %v\n", resp.RateLimitRemaining())
+		fmt.Printf("Rate limit reset:     %v\n", resp.RateLimitReset())
+	} else {
+		fmt.Printf("Could not parse rate limit from response.\n")
+	}
+	fmt.Printf("end PrintRateLimit \n")
+}
